2b08e617170927eaecc89940cbf27810d7f47002
yeison <yeisonrodriguezpacheco@gmail.com>
2019-10-02 01:52:54 -0600

    TAREA 3 TERMINADA

--- Tarea3/YEISON_RODRIGUEZ_PACHECO_3.asm
+++ Tarea3/YEISON_RODRIGUEZ_PACHECO_3.asm
@@ -1,15 +1,19 @@
 ;*******************************************************************************
-;                               TAREA 3
-;                          VARIAS SUBRUTINAS
+;                                 TAREA 3
+;                          SUBRUTINAS DEL DEBUG 12
 ;*******************************************************************************
 ;
 ;       UNIVERSIDAD DE COSTA RICA
-;       FECHA x/x/19
+;       FECHA 2/10/19
 ;       AUTOR: YEISON RODRIGUEZ PACHECO B56074
 ;       COREREO: yeisonrodriguezpacheco@gmail.com
 ;
 ;
-; Descripcion:
+; Descripcion: Este programa se encarga de analizar un conjunto de datos de 1
+; byte almacenados en DATOS con longitud LONG (1 byte), el programa pide al usua-
+; rio la cantidad de numeros maxima a encontrar y lo almacena en una variable
+; tipo byte llamada CANT. Seguidamente mostrara en pantalla las raices cuadradas
+; de los numeros encontrados en DATOS.
 
 
 
@@ -17,89 +21,115 @@
 ;                        DECLARACION ESTRUCTURAS DE DATOS
 ;*******************************************************************************
         ORG $1000
-;**********************************
-; DECLARACIONES GENERALES
-;**********************************
-ENTERO: ds 10
+LONG:   db 11
+CANT:   ds 1
+CONT:   ds 1
+
+        ORG $1020
+DATOS:  db 4,9,18,4,27,63,12,32,36,15,36,121,144,169,196,225,230
+
+        ORG $1040
 CUAD:   db 4,9,16,25,36,49,64,81,100,121,144,169,196,225
-LONG:   db 10
-DATOS:  db 4,9,18,4,27,63,12,32,36,15
 
-CANT:   db 7
+        ORG $1100
+ENTERO: ds 13
 
 ;**********************************
-; DECLARACIONES SUBRUTINA RAIZ
+; DECLARACIONES in/out DEBUG 12
 ;**********************************
+GETCHAR: EQU $EE84
+PUTCHAR: EQU $EE86
+printf:  EQU $EE88
+FIN:     EQU $0
+CR:      EQU $0D
+LF:      EQU $0A
+
+;**********************************
+; DECLARACIONES EXTRAS
+;**********************************
+        ORG $1300
 H__:    ds 1
 X__:    ds 1
 
-;**********************************
-; DECLARACIONES SUBRUTINA BUSCAR
-;**********************************
-CONT:   ds 1
+Msj_0:    fcc "INGRESE EL VALOR DE CANT (ENTRE 1 Y 99):"
+          db FIN
+Msj_null: fcc ""
+          db CR,LF,CR,LF,FIN
+Msj_1:    fcc "LA CANTIDAD DE NUMEROS ENCONTRADOS : %i"
+          db CR,LF,CR,LF,FIN
+Msj_2:    fcc "ENTERO: "
+          db FIN
+Msj_3:    fcc "%i, "
+          db FIN
+Msj_4:    fcc "%i"
+          db CR,LF,CR,LF,FIN
+          
+          
+          
 ;*******************************************************************************
 ;                              PROGRAMA PRINCIPAL
 ;*******************************************************************************
-        ; Raiz
+
 	ORG $2000
         LDS #$3000
-        LDX #CUAD
-LOOP:
-        LDAA 1,X+
-        PSHX
-        PSHA
-        JSR RAIZ
-        PULA
-        PULX
-        BRA LOOP
-        
-        ; Buscar
-        ORG $2100
-        LDS #$3000
+main:
+	JSR LEER_CANT
         JSR BUSCAR
-        BRA *
+        JSR Print_RESULT
+        BRA main
+        
+
 ;*******************************************************************************
-;                                SUBRUTINA RAIZ
+;                                SUBRUTINA LEER_CANT
 ;*******************************************************************************
 
-; Esta subrutina recibe un numero de 1 byte como parametro de entrada, a este
-; numero se le calcula la raiz cuadrada y el resultado tambien es devuelto por
-; la pila en un dato de 1 byte, que seria la raiz cuadrada del dato.
-; Esta funcion modifica todos los registros.
-
-; Calcula la raiz cuadrada de un numero
-
-; Parametros que usa
-; H__ Variable tipo byte. Uso igual al del enunciado de la tarea (raiz)
-; X__ Variable tipo byte. Uso igual al del enunciado de la tarea (raiz)
-
-        ORG $1100
-RAIZ:
-        PULY             ;Guardando direccion retorno
-        MOVB #1,H__
-        PULA
-        STAA X__         ; Guardando numero a calcular raiz en X__
+LEER_CANT:
+        LDX #0
+        LDD #Msj_0
+        JSR [printf,x]  ; Imprimiendo "INGRESE EL VALOR DE CANT (ENTRE 1 Y 99):"
+        LDX #0
+        LDAA #2
+Leyendo_dato:
         PSHA
-RAIZ_main_loop:
+        JSR [GETCHAR,x]
+        PULA
+        CMPB #$30
+        BMI Leyendo_dato         ; Verificando si es menor que 0
+        CMPB #$3a
+        BPL Leyendo_dato         ; Verificando si es mayor que 9
+        DBEQ A,Procesar
+	LDX #0
+        JSR [PUTCHAR,x]
+        STAB CANT
+        BRA Leyendo_dato
+Procesar:
+        LDAA CANT                      ; Verificando si ambos son 0, si es asi
+        CMPA #$30                      ; vuelve a pedir el segundo numero
+        BNE Continuar
+        CMPB #$30                      ; vuelve a pedir el segundo numero
+        BNE Continuar
+        LDAA #1
+        BRA Leyendo_dato
+Continuar:
+	LDX #0
+        JSR [PUTCHAR,x]
+        SUBB #$30       		; calculando el numero en binario
+        PSHB                            ;guardando el numero
+        LDAB #10
+        LDAA CANT
+        SUBA #$30                       ; calculando valor binario del num mas signif
+        MUL
+        STAB CANT
         PULB
-        CMPB H__
-        BEQ RAIZ_Fin
-        ADDB H__
-        LSRB              ; Dividiendo por 2
-        PSHB              ; Resultado se guarda en pila
-        TFR B,X            ; Cargando datos de division
-        CLRA
-        LDAB X__
-        IDIV
-        XGDX
-        STAB H__          ; Guardando resultado en H__
-        BRA RAIZ_main_loop
-RAIZ_Fin:
-        PSHB            ; Valor a retornar
-        PSHY            ; Direccion retorno
+        ADDB CANT
+        STAB CANT
+        LDX #0         ; Imprimiendo "INGRESE EL VALOR DE CANT (ENTRE 1 Y 99):";Imprimiendo espacios de linea
+        LDD #Msj_null
+        JSR [printf,x]
         RTS
-
-
+        
+        
+        
 
 ;*******************************************************************************
 ;                                SUBRUTINA BUSCAR
@@ -114,6 +144,7 @@ RAIZ_Fin:
 BUSCAR:
         LDX #DATOS      ; Inicializando datos
         LDAB LONG
+        CLR CONT
 BUSCAR_Main_loop:
         LDAA CONT       ; Terminando el programa si ya se encontraron CANT datos
         CMPA CANT
@@ -144,3 +175,102 @@ BUSCAR_Dec_cont:
         BRA BUSCAR_Main_loop
 BUSCAR_FIN:
         RTS
+        
+        
+        
+        
+        
+;*******************************************************************************
+;                                SUBRUTINA RAIZ
+;*******************************************************************************
+
+; Esta subrutina recibe un numero de 1 byte como parametro de entrada, a este
+; numero se le calcula la raiz cuadrada y el resultado tambien es devuelto por
+; la pila en un dato de 1 byte, que seria la raiz cuadrada del dato.
+; Esta funcion modifica todos los registros.
+
+; Calcula la raiz cuadrada de un numero
+
+; Parametros que usa
+; H__ Variable tipo byte. Uso igual al del enunciado de la tarea (raiz)
+; X__ Variable tipo byte. Uso igual al del enunciado de la tarea (raiz)
+
+RAIZ:
+        PULY             ;Guardando direccion retorno
+        MOVB #1,H__
+        PULA
+        STAA X__         ; Guardando numero a calcular raiz en X__
+        PSHA
+RAIZ_main_loop:
+        PULB
+        CMPB H__
+        BEQ RAIZ_Fin
+        ADDB H__
+        LSRB              ; Dividiendo por 2
+        PSHB              ; Resultado se guarda en pila
+        TFR B,X            ; Cargando datos de division
+        CLRA
+        LDAB X__
+        IDIV
+        XGDX
+        STAB H__          ; Guardando resultado en H__
+        BRA RAIZ_main_loop
+RAIZ_Fin:
+        PSHB            ; Valor a retornar
+        PSHY            ; Direccion retorno
+        RTS
+
+
+
+        
+;*******************************************************************************
+;                                SUBRUTINA Print_RESULT
+;*******************************************************************************
+;Descripcion: Esta subrutina se encarga de imprimir la cantidad de numeros con
+; raiz cuadrada encontrados, ademas de que imprime el resultado de estas raices.
+; Se utiliza la subrutina printf del DEBUG12.
+
+Print_RESULT:
+        LDX #0
+        CLRA            ; Guardando la cantidad de datos encontrados en pila para
+        LDAB CONT       ; desplegar en pantalla
+        PSHD
+        LDD #Msj_1
+        JSR [printf,x]  ; Imprimiendo mensaje "LA CANTIDAD DE NUMEROS ENCONTRADOS : %i"
+        LEAS 2,SP
+        LDX #0
+        LDD #Msj_2
+        JSR [printf,x]   ; Imprimiendo mensaje "ENTERO: "
+        LDY #ENTERO
+        LDAB CONT
+Print_imprimiendo:       ; Imprimiendo numeros resultados
+        DBEQ B,Print_impr_final ; Si es el ultimo numero, salta
+        LDX #0
+        CLRA                    ; parte alta en 0 para cargar numero de 1 byte
+        PSHB                    ; en el acumulador D
+        LDAB 1,Y+               ; numero en parte baja de D
+        PSHY                    ; GUARDANDO direccion de imprimir sig num en pila
+        PSHD
+        LDD #Msj_3
+        JSR [printf,x]   ; Imprimiendo Mensaje "%i,"
+        LEAS 2,SP
+        PULY             ; Retornar direccion de imprimir sig numero de pila
+        PULB             ; Retornando contador
+        BRA Print_imprimiendo
+Print_impr_final:             ; Imprimiendo ultimo numero que no lleva ,
+        LDX #0
+        CLRA
+        LDAB 0,Y
+        PSHD
+        LDD #Msj_4
+        JSR [printf,x]   ;Imprimiendo Mensaje "%i"
+        LEAS 2,SP
+        RTS             ; Retornando
+        
+        
+        
+        
+        
+
+        
+        
